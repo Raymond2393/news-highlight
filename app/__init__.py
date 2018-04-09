3f9960a2b4bf05939a424fc2ad304d26973a92a1
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-09 10:45:57 +0300

    add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
 
 # Initializing application
-app = Flask(__name__)
+    app = Flask(__name__)
 
-from app import views
+    # creating the app configuration
+    app.config.from_object(DevConfig[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+
+    return app
